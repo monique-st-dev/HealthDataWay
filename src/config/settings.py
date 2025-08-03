28ce12055acf88f689fd83fcb586ce1d6a16631c
Monik Stoitceva <monique.st.dev@gmail.com>
2025-08-04 02:11:27 +0300

    Remove email config from settings and .env for deployment

--- src/config/settings.py
+++ src/config/settings.py
@@ -180,15 +180,6 @@ LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/accounts/logout/'
 
 
-# Email configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 # Celery Configuration
